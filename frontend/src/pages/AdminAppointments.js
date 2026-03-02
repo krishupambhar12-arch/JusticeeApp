553ee2e1b634fdbefcf32d94ccf7c2dcf6beb511
krishu <krishupambhar12@gmail.com>
2026-03-02 23:22:26 +0530

    2/3/26

--- frontend/src/pages/AdminAppointments.js
+++ frontend/src/pages/AdminAppointments.js
@@ -3,9 +3,6 @@ import { API } from '../config/api';
 import AdminSidebar from '../components/AdminSidebar';
 import '../styles/adminAppointments.css';
 
-
-
-
 const AdminAppointments = () => {
   const [appointments, setAppointments] = useState([]);
   const [loading, setLoading] = useState(false);
